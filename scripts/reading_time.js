d7ff5643108c32bfcba78f0301bb71fbbfd7d925
Rafael Lima <akafael@users.noreply.github.com>
2017-08-19 15:21:46 -0300

    Create reading_time.js

---
+++ scripts/reading_time.js
@@ -0,0 +1,82 @@
+/**
+ * Script para indicar o tempo de leitura de um texto
+ * inspirado no site: http://www.putasacada.com.br/tempo/
+ * @author Rafael Lima
+ */
+ 
+/**
+ * Contador de Palavras
+ * @version 0.1
+ */
+function wordCounter(text){
+  // Remove espaços em branco usando regex e quebra palavras em uma array
+  var words = text.replace(/\s+/, " ").split(" ");
+  var wordCount = 0;
+
+  // Pega Quantidade de Palavras
+  for (i=0; i< words.length; i++){if(words[i] != ''){wordCount++;}}
+
+  return wordCount;
+}
+ 
+/**
+ * Tempo de leitura
+ * @version 0.2
+ */
+ function readingTimeMin(text, mode){
+   // Speed words/min
+   var readingMode = {slow: 130, avg:150, fast:200};
+   var timeMinutes = 0;
+   var readingSpeed = readingMode.avg;
+   
+   switch(mode){
+      case "slow":
+        readingSpeed = readingMode.slow;
+      break;
+      
+      case "fast":
+        readingSpeed = readingMode.fast;
+      break;
+      
+      case "avg":
+      default:
+        readingSpeed = readingMode.avg;
+      break;
+   }
+   
+   timeMinutes = readingSpeed*wordCounter(text);
+   
+   return timeMinutes;
+ }
+
+
+/**
+ * Tempo de leitura
+ * @version 0.2
+ */
+ function readingTimeSec(text, mode){
+   // Speed words/min
+   var readingMode = {slow: 1.0, avg:2.5, fast:3};
+   var timeSeconds = 0;
+   var readingSpeed = readingMode.avg;
+   
+   switch(mode){
+      case "slow":
+        readingSpeed = readingMode.slow;
+      break;
+      
+      case "fast":
+        readingSpeed = readingMode.fast;
+      break;
+      
+      case "avg":
+      default:
+        readingSpeed = readingMode.avg;
+      break;
+   }
+   
+   timeSeconds = readingSpeed*wordCounter(text);
+   
+   return timeSeconds;
+ }
+read
